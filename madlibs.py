9fd33d33a5f66726a0e7324c1f81d022667e492e
sudarshan284 <sudarshanthakur18001@gmail.com>
2023-08-02 22:04:59 +0530

    madlib project done

---
+++ madlibs.py
@@ -0,0 +1,6 @@
+verb = input("Verb :")
+prefix = input("Prefix :")
+suffix = input("suffix:")
+
+madlib = f"today is a very tiring day for {verb} ! This is so bad for us as we cant help {prefix} {suffix}"
+print(madlib)
